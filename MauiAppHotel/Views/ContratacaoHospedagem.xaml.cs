8c0d05dbe4b8ceb74f9dca9e110969487edc9f68
Tchovis69 <meleroenzogustavo@gmail.com>
2025-11-02 23:29:45 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,29 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+    private void Button_clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Sobre());
+
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Ops", ex.Message, "OK");
+
+        }
+
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+}
