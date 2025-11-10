b31c356f73a94ca1323d56f0d1d5ff9f3232bd3a
VictorHabcd <arturlbo0@gmail.com>
2025-11-10 00:45:49 -0300

    Update HospedagemContratada.xaml.cs

--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -2,8 +2,20 @@ namespace MauiAppHotel.Views;
 
 public partial class HospedagemContratada : ContentPage
 {
-	public HospedagemContratada()
-	{
-		InitializeComponent();
-	}
+    public HospedagemContratada()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+        try 
+        {
+            Navigation.PushAsync(new ContratacaoHospedagem());
+        } 
+        catch (Exception ex)
+        {
+            DisplayAlert("ops", ex.Message, "OK");
+        }
+    }
 }
